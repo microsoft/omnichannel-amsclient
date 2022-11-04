2603d4359cb7d6ca4c4608031dd0ec2c081e7130
Edward Tran <edtran@microsoft.com>
2022-11-04 00:55:50 -0700

    Add runtimeId & documentId in log data interface

--- src/AMSLogData.ts
+++ src/AMSLogData.ts
@@ -1,5 +1,7 @@
 export default interface AMSLogData {
+    AMSClientRuntimeId: string;
     ChatId: string;
+    DocumentId?: string;
     ElapsedTimeInMilliseconds?: number;
     Event?: string;
     Description?: string;
