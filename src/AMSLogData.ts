852ea2be0bfa877096beb0de5e62818924aa8c9f
Edward Tran <edtran@microsoft.com>
2023-01-31 03:36:06 -0500

    Add MimeType as additional property

--- src/AMSLogData.ts
+++ src/AMSLogData.ts
@@ -2,6 +2,7 @@ export default interface AMSLogData {
     AMSClientRuntimeId: string;
     ChatId: string;
     DocumentId?: string;
+    MimeType?: string;
     ElapsedTimeInMilliseconds?: number;
     Event?: string;
     Description?: string;
